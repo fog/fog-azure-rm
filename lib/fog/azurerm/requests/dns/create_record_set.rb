2b18ddc2da1fad73cc4035495b4fd45e27ec9f76
Adnan Khalil <adnan.khalil@confiz.com>
2016-03-31 20:05:31 +0500

    removing libraries and constants file
    Summary: Incorporated changes recommended by Omair.
    
    Test Plan: NA
    
    Reviewers: shaffan.chaudhry, abbas.sheikh, samawia.moin
    
    Subscribers: omair.sajid
    
    Differential Revision: http://phabricator.confiz.com/D6755

--- lib/fog/azurerm/requests/dns/create_record_set.rb
+++ lib/fog/azurerm/requests/dns/create_record_set.rb
@@ -3,7 +3,51 @@ module Fog
     class AzureRM
       class Real
         def create_record_set(dns_resource_group, zone_name, record_set_name, records, record_type, ttl)
-          @record_set.set_records_on_record_set(dns_resource_group, zone_name, record_set_name, records, record_type, ttl)
+          resource_url = "#{AZURE_RESOURCE}/subscriptions/#{@subscription_id}/resourceGroups/#{dns_resource_group}/providers/Microsoft.Network/dnsZones/#{zone_name}/#{record_type}/#{record_set_name}?api-version=2015-05-04-preview"
+          puts "Creating/Updating RecordSet #{record_set_name} of type '#{record_type}' in zone #{zone_name}"
+
+          case record_type
+            when 'A'
+              a_type_records_array = Array.new
+              records.each do |ip|
+                a_type_records_array.push({'ipv4Address' => ip})
+              end
+              body = {
+                  :location => 'global',
+                  :tags => '',
+                  :properties => {
+                      :TTL => ttl,
+                      :ARecords => a_type_records_array
+                  }
+              }
+            when 'CNAME'
+              body = {
+                  :location => 'global',
+                  :tags => '',
+                  :properties => {
+                      :TTL => ttl,
+                      :CNAMERecord => {
+                          'cname' => records.first # because cname only has 1 value and we know the object is an array passed in.
+                      }
+                  }
+              }
+          end
+
+          token = Fog::Credentials::AzureRM.get_token(@tenant_id, @client_id, @client_secret)
+          begin
+            dns_response = RestClient.put(
+                resource_url,
+                body.to_json,
+                accept: 'application/json',
+                content_type: 'application/json',
+                authorization: token
+            )
+            puts "RecordSet #{record_set_name} Created/Updated Successfully!"
+          rescue Exception => e
+            puts "Exception setting #{record_type} records for the record set: #{record_set_name}"
+            msg = "AzureDns::RecordSet - Exception is: #{e.message}"
+            fail msg
+          end
         end
       end
 
--- lib/fog/azurerm/requests/dns/list_record_sets.rb
+++ lib/fog/azurerm/requests/dns/list_record_sets.rb
@@ -3,7 +3,26 @@ module Fog
     class AzureRM
       class Real
         def list_record_sets(dns_resource_group, zone_name)
-          @record_set.list_record_sets(dns_resource_group, zone_name)
+          resource_url = "#{AZURE_RESOURCE}/subscriptions/#{@subscription_id}/resourceGroups/#{dns_resource_group}/providers/Microsoft.Network/dnsZones/#{zone_name}/recordsets?api-version=2015-05-04-preview"
+          token = Fog::Credentials::AzureRM.get_token(@tenant_id, @client_id, @client_secret)
+          begin
+            dns_response = RestClient.get(
+                resource_url,
+                accept: 'application/json',
+                content_type: 'application/json',
+                authorization: token)
+            response_hash = JSON.parse(dns_response)
+            response_hash['value']
+          rescue RestClient::Exception => e
+            body = JSON.parse(e.http_body)
+            if body.key?('error')
+              body = body['error']
+              msg = "Exception fetching record_sets: #{body['code']}, #{body['message']}"
+            else
+              msg = "Exception fetching record_sets: #{body['code']}, #{body['message']}"
+            end
+            fail msg
+          end
         end
       end
 

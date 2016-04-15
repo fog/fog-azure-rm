b0d62116af4b5ac177c5cab7091c51c7feea885f
samawia-moin-confiz <samawiamoin@confiz.com>
2016-04-15 11:02:40 +0500

    Minor Bug fix

--- lib/fog/azurerm/models/storage/storage_accounts.rb
+++ lib/fog/azurerm/models/storage/storage_accounts.rb
@@ -36,9 +36,16 @@ module Fog
         end
 
         def check_name_availability(name)
+          params = Azure::ARM::Storage::Models::StorageAccountCheckNameAvailabilityParameters.new
+          params.name = name
+          params.type = 'Microsoft.Storage/storageAccounts'
           puts "Checking Name availability: #{name}."
-          service.check_storage_account_name_availability(name)
-          puts "Name: #{name} is available."
+          name_available_obj = service.check_storage_account_name_availability(params)
+          if name_available_obj['nameAvailable'] == "true"
+            puts "Name: #{name} is available."
+          else
+            puts "Name: #{name} is not available."
+          end
         end
       end
     end
--- lib/fog/azurerm/requests/storage/check_storage_account_name_availability.rb
+++ lib/fog/azurerm/requests/storage/check_storage_account_name_availability.rb
@@ -3,13 +3,15 @@ module Fog
     class AzureRM
       # This class provides the actual implemention for service calls.
       class Real
-        def check_storage_account_name_availability(name)
-          @storage_mgmt_client.storage_accounts.check_name_availability(name)
+        def check_storage_account_name_availability(params)
+          promise = @storage_mgmt_client.storage_accounts.check_name_availability(params)
+          result = promise.value!
+          Azure::ARM::Storage::Models::CheckNameAvailabilityResult.serialize_object(result.body)
         end
       end
       # This class provides the mock implementation for unit tests.
       class Mock
-        def check_storage_account_name_availability(name)
+        def check_storage_account_name_availability(params)
         end
       end
     end

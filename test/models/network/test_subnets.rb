a676539a701a72732236346102d9e25259c4cfa8
Adnan Khalil <adnan.khalil@confiz.com>
2016-04-27 17:01:28 +0500

    minitest unit testing for virtual_network and network_interfaces

--- test/models/network/test_subnets.rb
+++ test/models/network/test_subnets.rb
@@ -42,6 +42,7 @@ class TestSubnets < Minitest::Test
     response = [ApiStub::Models::Network::Subnet.create_subnet_response]
     @service.stub :list_subnets, response do
       assert_instance_of Fog::Network::AzureRM::Subnet, @subnets.get('fog-test-subnet')
+      assert (@subnets.get('wrong-name')).nil?, true
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,63 +22,90 @@ end
 
 def server(service)
   Fog::Compute::AzureRM::Server.new(
-    name: 'fog-test-server',
-    location: 'West US',
-    resource_group: 'fog-test-rg',
-    vm_size: 'Basic_A0',
-    storage_account_name: 'shaffanstrg',
-    username: 'shaffan',
-    password: 'Confiz=123',
-    disable_password_authentication: false,
-    network_interface_card_id: '/subscriptions/67f2116d-4ea2-4c6c-b20a-f92183dbe3cb/resourceGroups/shaffanRG/providers/Microsoft.Network/networkInterfaces/testNIC',
-    publisher: 'Canonical',
-    offer: 'UbuntuServer',
-    sku: '14.04.2-LTS',
-    version: 'latest',
-    service: service
+      name: 'fog-test-server',
+      location: 'West US',
+      resource_group: 'fog-test-rg',
+      vm_size: 'Basic_A0',
+      storage_account_name: 'shaffanstrg',
+      username: 'shaffan',
+      password: 'Confiz=123',
+      disable_password_authentication: false,
+      network_interface_card_id: '/subscriptions/67f2116d-4ea2-4c6c-b20a-f92183dbe3cb/resourceGroups/shaffanRG/providers/Microsoft.Network/networkInterfaces/testNIC',
+      publisher: 'Canonical',
+      offer: 'UbuntuServer',
+      sku: '14.04.2-LTS',
+      version: 'latest',
+      service: service
   )
 end
 
 def availability_set(service)
   Fog::Compute::AzureRM::AvailabilitySet.new(
-    name: 'availability-set',
-    location: 'West US',
-    resource_group: 'fog-test-rg',
-    service: service
+      name: 'availability-set',
+      location: 'West US',
+      resource_group: 'fog-test-rg',
+      service: service
   )
 end
 
 def resource_group(service)
   Fog::Resources::AzureRM::ResourceGroup.new(
-    name: 'fog-test-rg',
+      name: 'fog-test-rg',
+      location: 'West US',
+      service: service
+  )
+end
+
+def public_ip(service)
+  Fog::Network::AzureRM::PublicIp.new(
+    name: 'fog-test-public-ip',
+    resource_group: 'fog-test-rg',
     location: 'West US',
+    type: 'Dynamic',
+    service: service
+  )
+end
+
+def subnet(service)
+  Fog::Network::AzureRM::Subnet.new(
+    name: 'fog-test-subnet',
+    id: '/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/virtualNetworks/fog-test-virtual-network/subnets/fog-test-subnet',
+    resource_group: 'fog-test-rg',
+    virtual_network_name: 'fog-test-virtual-network',
+    properties: nil,
+    addressPrefix: '10.1.0.0/24',
+    networkSecurityGroupId: "/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/networkSecurityGroups/fog-test-network-security-group",
+    routeTableId: "/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/routeTables/fog-test-route-table",
+    ipConfigurations:[
+        {id: "/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/networkInterfaces/fog-test-network-interface/ipConfigurations/fog-test-ip-configuration"}
+    ],
     service: service
   )
 end
 
 def virtual_network(service)
   Fog::Network::AzureRM::VirtualNetwork.new(
-    name: 'fog-test-virtual-network',
-    id: '/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/virtualNetworks/fog-test-virtual-network',
-    resource_group: 'fog-test-rg',
-    location: 'West US',
-    dns_list: '10.1.0.5,10.1.0.6',
-    subnet_address_list: '10.1.0.0/24',
-    network_address_list: '10.1.0.0/16,10.2.0.0/16',
-    properties: nil,
-    service: service
+      name: 'fog-test-virtual-network',
+      id: '/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/virtualNetworks/fog-test-virtual-network',
+      resource_group: 'fog-test-rg',
+      location: 'West US',
+      dns_list: '10.1.0.5,10.1.0.6',
+      subnet_address_list: '10.1.0.0/24',
+      network_address_list: '10.1.0.0/16,10.2.0.0/16',
+      properties: nil,
+      service: service
   )
 end
 
 def network_interface(service)
   Fog::Network::AzureRM::NetworkInterface.new(
-    name: 'fog-test-network-interface',
-    location: 'West US',
-    resource_group: 'fog-test-rg',
-    subnet_id: '/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/virtualNetworks/fog-test-virtual-network/subnets/fog-test-subnet',
-    ip_configuration_name: 'fog-test-ip-configuration',
-    private_ip_allocation_method: 'fog-test-private-ip-allocation-method',
-    properties: nil,
-    service: service
+      name: 'fog-test-network-interface',
+      location: 'West US',
+      resource_group: 'fog-test-rg',
+      subnet_id: '/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/virtualNetworks/fog-test-virtual-network/subnets/fog-test-subnet',
+      ip_configuration_name: 'fog-test-ip-configuration',
+      private_ip_allocation_method: 'fog-test-private-ip-allocation-method',
+      properties: nil,
+      service: service
   )
 end

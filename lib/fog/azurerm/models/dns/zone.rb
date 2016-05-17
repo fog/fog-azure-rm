39725aeacc3c3638ebbab19b88591b49239b45a1
samawia-moin-confiz <samawiamoin@confiz.com>
2016-05-17 16:32:38 +0500

    Added fog parser

--- lib/fog/azurerm/models/dns/zone.rb
+++ lib/fog/azurerm/models/dns/zone.rb
@@ -7,11 +7,31 @@ module Fog
         identity :name
         attribute :id
         attribute :resource_group
+        attribute :location
+        attribute :type
+        attribute :tags
+        attribute :etag
+        attribute :number_of_record_sets
+        attribute :max_number_of_recordsets
+
+        def self.parse(zone)
+          hash = {}
+          hash['id'] = zone['id']
+          hash['name'] = zone['name']
+          hash['resource_group'] = zone['resource_group']
+          hash['location'] = zone['location']
+          hash['type'] = zone['type']
+          hash['tags'] = zone['tags']
+          hash['etag'] = zone['etag']
+          hash['number_of_record_sets'] = zone['numberOfRecordSets']
+          hash['max_number_of_recordsets'] = zone['maxNumberOfRecordSets']
+        end
 
         def save
           requires :name
           requires :resource_group
-          service.create_zone(resource_group, name)
+          zone = service.create_zone(resource_group, name)
+          merge_attributes(Fog::DNS::AzureRM::Zone.parse(zone))
         end
 
         def destroy

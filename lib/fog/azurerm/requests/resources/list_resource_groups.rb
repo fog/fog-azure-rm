6d660b78d3c225e4083aaafac497035f9d620df5
samawia-moin-confiz <samawiamoin@confiz.com>
2016-05-24 11:27:36 +0500

    Peer review changes

--- lib/fog/azurerm/requests/resources/list_resource_groups.rb
+++ lib/fog/azurerm/requests/resources/list_resource_groups.rb
@@ -7,9 +7,7 @@ module Fog
             promise = @rmc.resource_groups.list
             result = promise.value!
             result.body.next_link = ''
-            value = Azure::ARM::Resources::Models::ResourceGroupListResult.serialize_object(result.body)['value']
-            puts "Result: #{value}"
-            value
+            Azure::ARM::Resources::Models::ResourceGroupListResult.serialize_object(result.body)['value']
           rescue  MsRestAzure::AzureOperationError => e
             msg = "Exception listing Resource Groups. #{e.body['error']['message']}"
             raise msg
@@ -30,13 +28,13 @@ module Fog
                 }
             },
             {
-                "location"=>"westus",
-                "id"=>"/subscriptions/########-####-####-####-############/resourceGroups/Fog_test_rg1",
-                "name"=>"Fog_test_rg1",
-                "properties"=>
-                    {
-                        "provisioningState"=>"Succeeded"
-                    }
+              "location"=>"westus",
+              "id"=>"/subscriptions/########-####-####-####-############/resourceGroups/Fog_test_rg1",
+              "name"=>"Fog_test_rg1",
+              "properties"=>
+                {
+                  "provisioningState"=>"Succeeded"
+                }
             }
           ]
         end

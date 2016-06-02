0ce7ce1510687f5a5f460613c9fb24e6498fa109
Shaffan <shaffan.chaudhry@confiz.com>
2016-06-02 16:30:53 +0500

    Updated create security group test to increase test coverage

--- test/requests/network/test_create_network_security_group.rb
+++ test/requests/network/test_create_network_security_group.rb
@@ -13,9 +13,21 @@ class TestCreateNetworkSecurityGroup < Minitest::Test
   def test_create_network_security_group_success
     mocked_response = ApiStub::Requests::Network::NetworkSecurityGroup.create_network_security_group_response
     expected_response = Azure::ARM::Network::Models::NetworkSecurityGroup.serialize_object(mocked_response.body)
+    security_rule = {
+	    name: 'testRule',
+      protocol: 'tcp',
+      source_port_range: '22',
+      destination_port_range: '22',
+      source_address_prefix: '0.0.0.0/0',
+      destination_address_prefix: '0.0.0.0/0',
+      access: 'Allow',
+      priority: '100',
+      direction: 'Inbound',
+      description: 'This is a test rule'
+	  }
     @promise.stub :value!, mocked_response do
       @network_security_groups.stub :begin_create_or_update, @promise do
-        assert_equal @service.create_network_security_group('fog-test-rg', 'fog-test-nsg', 'West US', []), expected_response
+        assert_equal @service.create_network_security_group('fog-test-rg', 'fog-test-nsg', 'West US', [security_rule]), expected_response
       end
     end
   end

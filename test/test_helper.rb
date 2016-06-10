40a20e9b81b86c25bb2f28af7dad23e3dfff9bf5
Adnan Khalil <adnan.khalil@confiz.com>
2016-06-10 18:32:57 +0500

    fix in helper class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -113,6 +113,7 @@ def network_interface(service)
     location: 'West US',
     resource_group: 'fog-test-rg',
     subnet_id: '/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/virtualNetworks/fog-test-virtual-network/subnets/fog-test-subnet',
+    public_ip_address_id: '/subscriptions/########-####-####-####-############/resourceGroups/fog-test-rg/providers/Microsoft.Network/publicIPAddresses/fog-test-public-ip',
     ip_configuration_name: 'fog-test-ip-configuration',
     private_ip_allocation_method: 'fog-test-private-ip-allocation-method',
     properties: nil,

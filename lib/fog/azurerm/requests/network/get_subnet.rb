2d7d008bb36e2f8791df324558d35e604f191e3f
Bilal Naeem <bilal.naeem@confiz.com>
2016-08-19 15:24:33 +0500

    Updated get method for subnet

--- lib/fog/azurerm/requests/network/get_subnet.rb
+++ lib/fog/azurerm/requests/network/get_subnet.rb
@@ -6,13 +6,11 @@ module Fog
         def get_subnet(resource_group, virtual_network_name, subnet_name)
           Fog::Logger.debug "Getting Subnet: #{subnet_name}."
           begin
-            promise = @network_client.subnets.get(resource_group, virtual_network_name, subnet_name)
-            result = promise.value!
+            result = @network_client.subnets.get(resource_group, virtual_network_name, subnet_name)
             Fog::Logger.debug "Subnet #{subnet_name} retrieved successfully."
-            Azure::ARM::Network::Models::Subnet.serialize_object(result.body)
+            result
           rescue  MsRestAzure::AzureOperationError => e
-            msg = "Exception getting Subnet #{subnet_name} in Resource Group: #{resource_group}. #{e.body['error']['message']}"
-            raise msg
+            raise Fog::AzureRm::OperationError.new(e)
           end
         end
       end
--- test/requests/network/test_get_subnet.rb
+++ test/requests/network/test_get_subnet.rb
@@ -4,29 +4,22 @@ require File.expand_path '../../test_helper', __dir__
 class TestGetSubnet < Minitest::Test
   def setup
     @service = Fog::Network::AzureRM.new(credentials)
-    client = @service.instance_variable_get(:@network_client)
-    @subnets = client.subnets
-    @promise = Concurrent::Promise.execute do
-    end
+    @client = @service.instance_variable_get(:@network_client)
+    @subnets = @client.subnets
   end
 
   def test_get_subnet_success
-    mocked_response = ApiStub::Requests::Network::Subnet.create_subnet_response
-    expected_response = Azure::ARM::Network::Models::Subnet.serialize_object(mocked_response.body)
-    @promise.stub :value!, mocked_response do
-      @subnets.stub :get, @promise do
-        assert_equal @service.get_subnet('fog-test-rg', 'fog-test-virtual-network', 'fog-test-subnet'), expected_response
-      end
+    mocked_response = ApiStub::Requests::Network::Subnet.create_subnet_response(@client)
+    @subnets.stub :get, mocked_response do
+      assert_equal @service.get_subnet('fog-test-rg', 'fog-test-virtual-network', 'fog-test-subnet'), mocked_response
     end
   end
 
   def test_get_subnet_exception_failure
-    response = -> { raise MsRestAzure::AzureOperationError.new(nil, nil, 'error' => { 'message' => 'mocked exception' }) }
-    @promise.stub :value!, response do
-      @subnets.stub :get, @promise do
-        assert_raises RuntimeError do
-          @service.get_subnet('fog-test-rg', 'fog-test-virtual-network', 'fog-test-subnet')
-        end
+    response = proc { raise MsRestAzure::AzureOperationError.new(nil, nil, 'error' => { 'message' => 'mocked exception' }) }
+    @subnets.stub :get, response do
+      assert_raises Fog::AzureRm::OperationError do
+        @service.get_subnet('fog-test-rg', 'fog-test-virtual-network', 'fog-test-subnet')
       end
     end
   end

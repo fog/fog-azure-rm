2ede9b95d0d2dab40c014b68343085b759905ad5
Bilal Naeem <bilal.naeem@confiz.com>
2016-09-05 14:24:58 +0500

    Travis file changed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,7 @@ require 'minitest/autorun'
 $LOAD_PATH.unshift(File.expand_path '../../lib', __FILE__)
 require File.expand_path '../../lib/fog/azurerm', __FILE__
 require File.expand_path '../api_stub', __FILE__
+
 def credentials
   {
     tenant_id: '<TENANT-ID>',

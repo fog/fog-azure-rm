15d526bce9670ba3ead1d01a8c198a69ba7e1211
Bilal Naeem <bilal.naeem@confiz.com>
2016-09-07 10:53:27 +0500

    Using Fog DSL to load services

--- lib/fog/azurerm.rb
+++ lib/fog/azurerm.rb
@@ -41,28 +41,6 @@ module Fog
     service(:network, 'Network')
     service(:compute, 'Compute')
     service(:application_gateway, 'ApplicationGateway')
-
-    def self.services
-      begin
-        array_of_services = []
-        ENV['BUNDLE_GEM'] = File.expand_path('../lib', File.dirname(__dir__))
-        gem_path = ENV['BUNDLE_GEM']
-        files = Dir.entries(File.join(gem_path, '/fog/azurerm')).select { |f| !File.directory? f }
-        files.each do |file|
-          next if file == 'config.rb'
-          next if file == 'credentials.rb'
-          next if file == 'docs'
-          next if file == 'models'
-          next if file == 'requests'
-          next if file == 'version.rb'
-          array_of_services.push(file.split('.').first.upcase)
-        end
-        array_of_services
-      rescue => e
-        Fog::Logger.warning(e.message)
-        raise e.message
-        # typically occurs if folder_to_count does not exist
-      end
-    end
+    service(:traffic_manager, 'TrafficManager')
   end
 end

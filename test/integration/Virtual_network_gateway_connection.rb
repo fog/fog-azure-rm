fe8c482a305cb8680316a49fbbb2865e39e0e73a
zeeshan-arshad-confiz <zeeshan.arshad@confiz.com>
2016-09-08 17:33:27 +0500

    fixed rubocop issues in integration tests

--- test/integration/Virtual_network_gateway_connection.rb
+++ test/integration/Virtual_network_gateway_connection.rb
@@ -9,17 +9,17 @@ require 'yaml'
 azure_credentials = YAML.load_file('credentials/azure.yml')
 
 resource = Fog::Resources::AzureRM.new(
-    tenant_id: azure_credentials['tenant_id'],
-    client_id: azure_credentials['client_id'],
-    client_secret: azure_credentials['client_secret'],
-    subscription_id: azure_credentials['subscription_id']
+  tenant_id: azure_credentials['tenant_id'],
+  client_id: azure_credentials['client_id'],
+  client_secret: azure_credentials['client_secret'],
+  subscription_id: azure_credentials['subscription_id']
 )
 
 network = Fog::Network::AzureRM.new(
-    tenant_id: azure_credentials['tenant_id'],
-    client_id: azure_credentials['client_id'],
-    client_secret: azure_credentials['client_secret'],
-    subscription_id: azure_credentials['subscription_id']
+  tenant_id: azure_credentials['tenant_id'],
+  client_id: azure_credentials['client_id'],
+  client_secret: azure_credentials['client_secret'],
+  subscription_id: azure_credentials['subscription_id']
 )
 
 ########################################################################################################################
@@ -27,131 +27,131 @@ network = Fog::Network::AzureRM.new(
 ########################################################################################################################
 
 resource.resource_groups.create(
-    name: 'TestRG-GC',
-    location: 'eastus'
+  name: 'TestRG-GC',
+  location: 'eastus'
 )
 
 network.virtual_networks.create(
-    name: 'testVnet',
-    location: 'eastus',
-    resource_group: 'TestRG-GC',
-    network_address_list: '10.1.0.0/16,10.2.0.0/16'
+  name: 'testVnet',
+  location: 'eastus',
+  resource_group: 'TestRG-GC',
+  network_address_list: '10.1.0.0/16,10.2.0.0/16'
 )
 
 network.virtual_networks.create(
-    name: 'testVnet2',
-    location: 'eastus',
-    resource_group: 'TestRG-GC',
-    network_address_list: '10.3.0.0/16,10.4.0.0/16'
+  name: 'testVnet2',
+  location: 'eastus',
+  resource_group: 'TestRG-GC',
+  network_address_list: '10.3.0.0/16,10.4.0.0/16'
 )
 
 network.subnets.create(
-    name: 'GatewaySubnet',
-    resource_group: 'TestRG-GC',
-    virtual_network_name: 'testVnet',
-    address_prefix: '10.2.0.0/24'
+  name: 'GatewaySubnet',
+  resource_group: 'TestRG-GC',
+  virtual_network_name: 'testVnet',
+  address_prefix: '10.2.0.0/24'
 )
 
 network.subnets.create(
-    name: 'GatewaySubnet2',
-    resource_group: 'TestRG-GC',
-    virtual_network_name: 'testVnet2',
-    address_prefix: '10.2.0.0/24'
+  name: 'GatewaySubnet2',
+  resource_group: 'TestRG-GC',
+  virtual_network_name: 'testVnet2',
+  address_prefix: '10.2.0.0/24'
 )
 
 network.public_ips.create(
-    name: 'mypubip',
-    resource_group: 'TestRG-GC',
-    location: 'eastus',
-    public_ip_allocation_method: 'Dynamic'
+  name: 'mypubip',
+  resource_group: 'TestRG-GC',
+  location: 'eastus',
+  public_ip_allocation_method: 'Dynamic'
 )
 
 network.public_ips.create(
-    name: 'mypubip2',
-    resource_group: 'TestRG-GC',
-    location: 'eastus',
-    public_ip_allocation_method: 'Dynamic'
+  name: 'mypubip2',
+  resource_group: 'TestRG-GC',
+  location: 'eastus',
+  public_ip_allocation_method: 'Dynamic'
 )
 
 network.virtual_network_gateways.create(
-    name: 'testnetworkgateway',
-    location: 'eastus',
-    tags: {
-        key1: 'value1',
-        key2: 'value2'
-    },
-    ip_configurations: [
-        {
-            name: 'default',
-            private_ipallocation_method: 'Dynamic',
-            public_ipaddress_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/publicIPAddresses/mypubip",
-            subnet_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/virtualNetworks/testVnet/subnets/GatewaySubnet",
-            private_ipaddress: nil
-        }
-    ],
-    resource_group: 'TestRG-GC',
-    sku_name: 'Standard',
-    sku_tier: 'Standard',
-    sku_capacity: 2,
-    gateway_type: 'vpn',
-    enable_bgp: true,
-    gateway_size: nil,
-    asn: 100,
-    bgp_peering_address: nil,
-    peer_weight: 3,
-    vpn_type: 'RouteBased',
-    vpn_client_address_pool: [],
-    gateway_default_site: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/localNetworkGateways/{local_network_gateway_name}"
+  name: 'testnetworkgateway',
+  location: 'eastus',
+  tags: {
+    key1: 'value1',
+    key2: 'value2'
+  },
+  ip_configurations: [
+    {
+      name: 'default',
+      private_ipallocation_method: 'Dynamic',
+      public_ipaddress_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/publicIPAddresses/mypubip",
+      subnet_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/virtualNetworks/testVnet/subnets/GatewaySubnet",
+      private_ipaddress: nil
+    }
+  ],
+  resource_group: 'TestRG-GC',
+  sku_name: 'Standard',
+  sku_tier: 'Standard',
+  sku_capacity: 2,
+  gateway_type: 'vpn',
+  enable_bgp: true,
+  gateway_size: nil,
+  asn: 100,
+  bgp_peering_address: nil,
+  peer_weight: 3,
+  vpn_type: 'RouteBased',
+  vpn_client_address_pool: [],
+  gateway_default_site: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/localNetworkGateways/{local_network_gateway_name}"
 )
 
 network.virtual_network_gateways.create(
-    name: 'testnetworkgateway2',
-    location: 'eastus',
-    tags: {
-        key1: 'value1',
-        key2: 'value2'
-    },
-    ip_configurations: [
-        {
-            name: 'default',
-            private_ipallocation_method: 'Dynamic',
-            public_ipaddress_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/publicIPAddresses/mypubip2",
-            subnet_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/virtualNetworks/testVnet2/subnets/GatewaySubnet",
-            private_ipaddress: nil
-        }
-    ],
-    resource_group: 'TestRG-GC',
-    sku_name: 'Standard',
-    sku_tier: 'Standard',
-    sku_capacity: 2,
-    gateway_type: 'vpn',
-    enable_bgp: true,
-    gateway_size: nil,
-    asn: 100,
-    bgp_peering_address: nil,
-    peer_weight: 3,
-    vpn_type: 'RouteBased',
-    vpn_client_address_pool: [],
-    gateway_default_site: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/localNetworkGateways/{local_network_gateway_name}"
+  name: 'testnetworkgateway2',
+  location: 'eastus',
+  tags: {
+    key1: 'value1',
+    key2: 'value2'
+  },
+  ip_configurations: [
+    {
+      name: 'default',
+      private_ipallocation_method: 'Dynamic',
+      public_ipaddress_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/publicIPAddresses/mypubip2",
+      subnet_id: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/virtualNetworks/testVnet2/subnets/GatewaySubnet",
+      private_ipaddress: nil
+    }
+  ],
+  resource_group: 'TestRG-GC',
+  sku_name: 'Standard',
+  sku_tier: 'Standard',
+  sku_capacity: 2,
+  gateway_type: 'vpn',
+  enable_bgp: true,
+  gateway_size: nil,
+  asn: 100,
+  bgp_peering_address: nil,
+  peer_weight: 3,
+  vpn_type: 'RouteBased',
+  vpn_client_address_pool: [],
+  gateway_default_site: "/subscriptions/#{azure_credentials['subscription_id']}/resourceGroups/TestRG-GC/providers/Microsoft.Network/localNetworkGateways/{local_network_gateway_name}"
 )
 
 ########################################################################################################################
 ######################                           Create Virtual Network Gateway Connection                   ###########
 ########################################################################################################################
 
-gateway_connection = network.virtual_network_gateway_connections.create(
-    name: 'testnetworkgateway2-to-testnetworkgateway',
-    location: 'eastus',
-    resource_group: 'TestRG-GC',
-    virtual_network_gateway1: {
-        name: 'testnetworkgateway2',
-        resource_group: 'TestRG-GC'
-    },
-    virtual_network_gateway2: {
-        name: 'testnetworkgateway',
-        resource_group: 'TestRG-GC'
-    },
-    connection_type: 'Vnet2Vnet'
+network.virtual_network_gateway_connections.create(
+  name: 'testnetworkgateway2-to-testnetworkgateway',
+  location: 'eastus',
+  resource_group: 'TestRG-GC',
+  virtual_network_gateway1: {
+    name: 'testnetworkgateway2',
+    resource_group: 'TestRG-GC'
+  },
+  virtual_network_gateway2: {
+    name: 'testnetworkgateway',
+    resource_group: 'TestRG-GC'
+  },
+  connection_type: 'Vnet2Vnet'
 )
 
 ########################################################################################################################

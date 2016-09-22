1f2208530991b73b8351d30f67db140b61213f23
Bilal Naeem <bilal.naeem@confiz.com>
2016-09-22 11:02:28 +0500

    Fixed hound offence

--- lib/fog/azurerm/requests/network/get_available_ipaddresses_count.rb
+++ lib/fog/azurerm/requests/network/get_available_ipaddresses_count.rb
@@ -6,7 +6,7 @@ module Fog
         def get_available_ipaddresses_count(subnet_name, address_prefix, ip_configurations_ids, express_route_enabled)
           Fog::Logger.debug "Getting free IP Address count of Subnet #{subnet_name}"
           total_ipaddresses = (2**(32 - address_prefix.split('/').last.to_i)) - 2
-          total_ipaddresses = total_ipaddresses - 3 if express_route_enabled
+          total_ipaddresses -= 3 if express_route_enabled
           used_ip_address = ip_configurations_ids.nil? ? 0 : ip_configurations_ids.count
           total_ipaddresses - used_ip_address
         end

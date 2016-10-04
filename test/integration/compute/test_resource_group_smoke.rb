6cc9652575bed930bef96c8fc099e07e59d6869c
samawia-moin-confiz <samawiamoin@confiz.com>
2016-10-04 15:00:16 +0500

    Test

--- test/integration/compute/test_resource_group_smoke.rb
+++ test/integration/compute/test_resource_group_smoke.rb
@@ -33,6 +33,7 @@ class TestResourceGroupSmoke < MiniTest::Test
     assert_instance_of Fog::Resources::AzureRM::ResourceGroup, resource_group
   end
 
+  
   def test_all_resource_groups
   skip
   end

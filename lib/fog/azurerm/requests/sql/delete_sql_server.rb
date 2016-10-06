365ae081003a3190814ee37f73f1f47550cd027d
wHaider <whaider@cmdagency.com>
2016-10-06 13:00:03 +0500

    Added support for SQL Server

---
+++ lib/fog/azurerm/requests/sql/delete_sql_server.rb
@@ -0,0 +1,34 @@
+module Fog
+  module Sql
+    class AzureRM
+      # Real class for Sql Server Request
+      class Real
+        def delete_sql_server(resource_group, name)
+          msg = "Deleting SQL Server: #{name}."
+          Fog::Logger.debug msg
+          resource_url = "#{AZURE_RESOURCE}/subscriptions/#{@subscription_id}/resourceGroups/#{resource_group}/providers/Microsoft.Sql/servers/#{name}?api-version=2014-04-01-preview"
+          begin
+            token = Fog::Credentials::AzureRM.get_token(@tenant_id, @client_id, @client_secret)
+            RestClient.delete(
+                  resource_url,
+                  accept: :json,
+                  content_type: :json,
+                  authorization: token
+            )
+            Fog::Logger.debug "SQL Server: #{name} deleted successfully."
+            true
+          rescue MsRestAzure::AzureOperationError => e
+            raise_azure_exception(e, JSON.parse(e.response)['message'])
+          end
+        end
+      end
+      # Mock class for Sql Server Request
+      class Mock
+        def delete_sql_server(*)
+          Fog::Logger.debug 'SQL Server {name} from Resource group {resource_group} deleted successfully.'
+          true
+        end
+      end
+    end
+  end
+end

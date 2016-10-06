7cb65160d844669a96f93e32ddbb5840989fe599
samawia-moin-confiz <samawiamoin@confiz.com>
2016-10-06 17:26:38 +0500

    Test

--- test/integration/compute/test_resource_group_smoke.rb
+++ test/integration/compute/test_resource_group_smoke.rb
@@ -5,10 +5,10 @@ require 'yaml'
 class TestResourceGroupSmoke < MiniTest::Test
   begin
     @@resource = Fog::Resources::AzureRM.new(
-        tenant_id: ENV['TENANT_ID'],
-        client_id: ENV['CLIENT_ID'],
-        client_secret: ENV['CLIENT_SECRET'],
-        subscription_id: ENV['SUBSCRIPTION_ID']
+        tenant_id: "5ad58990-1a8e-439a-9de6-804ed6e5f511",
+        client_id: "40f33832-e9f8-4524-97c4-9e66f059a087",
+        client_secret: "Hwc69ON3/FW6xo8zeoDPhjlg9Xg0kI0ZpiNUYjgGbcY=",
+        subscription_id: "67f2116d-4ea2-4c6c-b20a-f92183dbe3cb"
     )
     time = Time.now.to_f.to_s
     puts "Time: #{time}"
@@ -18,21 +18,22 @@ class TestResourceGroupSmoke < MiniTest::Test
     puts e
   end
 
-  Minitest.after_run do
-    begin
-      resource_group = @@resource.resource_groups.get(@@resource_group_name)
-      resource_group.destroy
-    rescue StandardError => e
-      puts e
-    end
-  end
+  # Minitest.after_run do
+  #   begin
+  #     resource_group = @resource.resource_groups.get(@resource_group_name)
+  #     resource_group.destroy
+  #   rescue StandardError => e
+  #     puts e
+  #   end
+  # end
 
   def setup
     @resource = @@resource
+    @resource_group_name = @@resource_group_name
   end
 
   def test_create_resource_group
-    resource_group = @resource.resource_groups.create(name: @@resource_group_name, location: 'eastus')
+    resource_group = @resource.resource_groups.create(name: @resource_group_name, location: 'eastus')
     assert_instance_of Fog::Resources::AzureRM::ResourceGroup, resource_group
   end
 
@@ -41,7 +42,7 @@ class TestResourceGroupSmoke < MiniTest::Test
   end
 
   def test_get_resource_group
-    resource_group = @resource.resource_groups.get(@@resource_group_name)
+    resource_group = @resource.resource_groups.get(@resource_group_name)
     assert_instance_of Fog::Resources::AzureRM::ResourceGroup, resource_group
   end
 end

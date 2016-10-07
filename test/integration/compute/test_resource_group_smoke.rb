be05ea803a8baffac9ea976fe44b66c7d5fd57f2
samawia-moin-confiz <samawiamoin@confiz.com>
2016-10-07 15:57:05 +0500

    Test

--- test/integration/compute/test_resource_group_smoke.rb
+++ test/integration/compute/test_resource_group_smoke.rb
@@ -11,24 +11,23 @@ class TestResourceGroupSmoke < MiniTest::Test
         subscription_id: ENV['SUBSCRIPTION_ID']
     )
     time = Time.now.to_f.to_s
-    puts "Time: #{time}"
     new_time = time.split(/\W+/).join
     @@resource_group_name = "fog-smoke-test-rg-#{new_time}"
   rescue StandardError => e
     puts e
   end
 
+  def setup
+    @resource = @@resource
+  end
+
   Minitest.after_run do
     begin
-      resource_group = @@resource.resource_groups.get(@@resource_group_name)
+      resource_group = @resource.resource_groups.get(@@resource_group_name)
       resource_group.destroy
     rescue StandardError => e
       puts e
     end
-  end
-
-  def setup
-    @resource = @@resource
   end
 
   def test_resource_group
@@ -37,15 +36,4 @@ class TestResourceGroupSmoke < MiniTest::Test
     resource_group = @resource.resource_groups.get(@@resource_group_name)
     assert_instance_of Fog::Resources::AzureRM::ResourceGroup, resource_group
   end
-
-
-  
-  # def test_all_resource_groups
-  # skip
-  # end
-  #
-  # def test_get_resource_group
-  #   resource_group = @resource.resource_groups.get(@@resource_group_name)
-  #   assert_instance_of Fog::Resources::AzureRM::ResourceGroup, resource_group
-  # end
 end

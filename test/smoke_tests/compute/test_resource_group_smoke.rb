9b62d3de703e4794d3ce0ad90b13b0e89b8c2e42
samawia-moin-confiz <samawiamoin@confiz.com>
2016-10-21 17:07:32 +0500

    Made changes

--- test/smoke_tests/compute/test_resource_group_smoke.rb
+++ test/smoke_tests/compute/test_resource_group_smoke.rb
@@ -6,9 +6,9 @@ class TestResourceGroupSmoke < MiniTest::Test
     def setup
       @resource = Fog::Resources::AzureRM.new(
         tenant_id: ENV['TENANT_ID'],
-        client_id: ENV['CLIENT_ID'],
-        client_secret: ENV['CLIENT_SECRET'],
-        subscription_id: ENV['SUBSCRIPTION_ID']
+          client_id: ENV['CLIENT_ID'],
+          client_secret: ENV['CLIENT_SECRET'],
+          subscription_id: ENV['SUBSCRIPTION_ID']
       )
       time = Time.now.to_f.to_s
       new_time = time.split(/\W+/).join
@@ -16,6 +16,7 @@ class TestResourceGroupSmoke < MiniTest::Test
     end
 
     def test_resource_group
+      puts "In Smoke tests"
       resource_group = @resource.resource_groups.create(name: @resource_group_name, location: 'eastus')
       assert_instance_of Fog::Resources::AzureRM::ResourceGroup, resource_group
 

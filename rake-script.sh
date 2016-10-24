218c38f4231f4bf9f43e159fc9fd349afad6c8fb
samawia-moin-confiz <samawiamoin@confiz.com>
2016-10-24 18:55:27 +0500

    change rake script

--- rake-script.sh
+++ rake-script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo $TRAVIS_BRANCH
-if [ $TRAVIS_BRANCH == 'develop' ]
+if [ $TRAVIS_BRANCH = 'develop' ]
 then
+  echo "In First check"
   rake cc_coverage
   rake integration_tests
 else
+  echo "In else check"
   rake cc_coverage
 fi

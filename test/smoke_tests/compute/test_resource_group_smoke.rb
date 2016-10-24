41baf928eea10f4e841928359cc8ee264eaf4f70
samawia-moin-confiz <samawiamoin@confiz.com>
2016-10-24 15:58:39 +0500

    Added line in rake script

--- test/smoke_tests/compute/test_resource_group_smoke.rb
+++ test/smoke_tests/compute/test_resource_group_smoke.rb
@@ -4,22 +4,22 @@ require 'minitest/autorun'
 class TestResourceGroupSmoke < MiniTest::Test
 
     def setup
-      if !ENV['TENANT_ID'].nil? && !ENV['CLIENT_ID'].nil? && !ENV['CLIENT_SECRET'].nil? && !ENV['SUBSCRIPTION_ID'].nil?
+      # if !ENV['TENANT_ID'].nil? && !ENV['CLIENT_ID'].nil? && !ENV['CLIENT_SECRET'].nil? && !ENV['SUBSCRIPTION_ID'].nil?
         @resource = Fog::Resources::AzureRM.new(
           tenant_id: ENV['TENANT_ID'],
             client_id: ENV['CLIENT_ID'],
             client_secret: ENV['CLIENT_SECRET'],
             subscription_id: ENV['SUBSCRIPTION_ID']
         )
-      else
-        azure_credentials = YAML.load_file('integration/credentials/azure.yml')
-        @resource = Fog::Resources::AzureRM.new(
-          tenant_id: azure_credentials['tenant_id'],
-          client_id: azure_credentials['client_id'],
-          client_secret: azure_credentials['client_secret'],
-          subscription_id: azure_credentials['subscription_id']
-        )
-      end
+      # else
+      #   azure_credentials = YAML.load_file('integration/credentials/azure.yml')
+      #   @resource = Fog::Resources::AzureRM.new(
+      #     tenant_id: azure_credentials['tenant_id'],
+      #     client_id: azure_credentials['client_id'],
+      #     client_secret: azure_credentials['client_secret'],
+      #     subscription_id: azure_credentials['subscription_id']
+      #   )
+      # end
       time = Time.now.to_f.to_s
       new_time = time.split(/\W+/).join
       @resource_group_name = "fog-smoke-test-rg-#{new_time}"

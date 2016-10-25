997f96192df744f40d22a8e47a70e36b61d7bfa0
samawia-moin-confiz <samawiamoin@confiz.com>
2016-10-25 10:25:40 +0500

    Changed file

--- rake-script.sh
+++ rake-script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo $TRAVIS_BRANCH
-echo ${ENV['CLIENT_ID']:0:2}
-if [ $TRAVIS_BRANCH = 'develop' ]
+echo ${#ENV['CLIENT_ID']}
+if [ $TRAVIS_BRANCH = 'master' ]
 then
-  echo ${ENV['CLIENT_ID']:0:2}
+  echo ${#ENV['CLIENT_ID']}
   echo "In First check"
   rake cc_coverage
   rake integration_tests
 else
-  echo ${ENV['CLIENT_ID']:0:2}
+  echo ${#ENV['CLIENT_ID']}
   echo "In else check"
   rake cc_coverage
 fi

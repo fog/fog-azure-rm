a59d907436a3918f89ae99fc148bc6541acc6d9b
Muhammad Zeeshan Arshad <zeeshan.arshad@confiz.com>
2016-11-08 17:30:59 +0500

    changed endpoint urls

--- lib/fog/azurerm/config.rb
+++ lib/fog/azurerm/config.rb
@@ -28,5 +28,5 @@ CLOUD = 'Azure'.freeze
 
 AZURE_GLOBAL_RM_ENDPOINT_URL = 'https://management.azure.com/'.freeze
 AZURE_CHINA_RM_ENDPOINT_URL = 'https://management.chinacloudapi.cn'.freeze
-GERMAN_CLOUD_RM_ENDPOINT_URL = 'https://management.usgovcloudapi.net'.freeze
-US_GOVERNMENT_RM_ENDPOINT_URL = 'https://management.microsoftazure.de'.freeze
+GERMAN_CLOUD_RM_ENDPOINT_URL = 'https://management.microsoftazure.de'.freeze
+US_GOVERNMENT_RM_ENDPOINT_URL = 'https://management.usgovcloudapi.net'.freeze

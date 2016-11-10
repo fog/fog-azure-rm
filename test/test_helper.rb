53130d4d87e2d4247474707709e51b985723eea4
Samawia Moin <samawia.moin@confiz.com>
2016-11-10 15:50:16 +0500

    changed file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,10 +6,16 @@ if ENV['COVERAGE']
   end
 end
 
-# if ENV['CODECLIMATE_REPO_TOKEN']
-#   require 'simplecov'
-#   SimpleCov.start
-# end
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  require 'simplecov'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[SimpleCov::Formatter::HTMLFormatter,CodeClimate::TestReporter::Formatter]
+
+  dir = File.join("..", "coverage")
+  SimpleCov.coverage_dir(dir)
+  SimpleCov.start CodeClimate::TestReporter.configuration.profile
+end
 
 require 'minitest/autorun'
 $LOAD_PATH.unshift(File.expand_path '../lib', __dir__)

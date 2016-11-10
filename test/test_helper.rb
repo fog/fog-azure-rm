ade3adad6f8f9e8ab8ec096aa4c8570bc1c7ec7e
Samawia Moin <samawia.moin@confiz.com>
2016-11-10 15:22:21 +0500

    Made changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,11 @@ if ENV['COVERAGE']
   end
 end
 
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require 'minitest/autorun'
 $LOAD_PATH.unshift(File.expand_path '../lib', __dir__)
 require File.expand_path '../lib/fog/azurerm', __dir__

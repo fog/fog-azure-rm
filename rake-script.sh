3e669edd8e2a002a5376a55e24490980a5c426d6
Samawia Moin <samawia.moin@confiz.com>
2016-11-11 10:28:28 +0500

    Minor change in sh script

--- rake-script.sh
+++ rake-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $TRAVIS_PULL_REQUEST != "true"]
+if [ $TRAVIS_PULL_REQUEST != "true" ]
 then
   rake cc_coverage
   export CODECLIMATE_REPO_TOKEN="b1401494baa004d90402414cb33a7fc6420fd3693e60c677a120ddefd7d84cfd"

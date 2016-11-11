61ab5a850e40f59fa2ddc51985335768874e4f70
Samawia Moin <samawia.moin@confiz.com>
2016-11-11 09:37:26 +0500

    Changed sh file

--- rake-script.sh
+++ rake-script.sh
@@ -2,6 +2,8 @@
 if [ $TRAVIS_PULL_REQUEST != "true" ]
 then
   rake cc_coverage
+  codeclimate-test-reporter --directory /home/travis/build/fog/fog-azure-rm/coverage
 else
   rake cc_coverage
+  codeclimate-test-reporter --directory /home/travis/build/fog/fog-azure-rm/coverage
 fi

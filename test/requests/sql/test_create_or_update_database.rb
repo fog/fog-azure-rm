57a8bf2b6eaa1a0e5f5f2f317b663ca2829c4d3b
Waqar Haider <waqar.haider@confiz.com>
2017-01-02 16:04:14 +0500

    Updated changelog and added changes suggested in peer review

--- test/requests/sql/test_create_or_update_database.rb
+++ test/requests/sql/test_create_or_update_database.rb
@@ -6,20 +6,20 @@ class TestCreateOrUpdateDatabase < Minitest::Test
     @service = Fog::Sql::AzureRM.new(credentials)
     @sql_manager_client = @service.instance_variable_get(:@sql_mgmt_client)
     @databases = @sql_manager_client.databases
-    @data_hash = ApiStub::Requests::Sql::SqlDatabase.database_hash
+    @database_hash = ApiStub::Requests::Sql::SqlDatabase.database_hash
   end
 
   def test_create_or_update_database_success
     database_response = ApiStub::Requests::Sql::SqlDatabase.create_database_response(@sql_manager_client)
     @databases.stub :create_or_update, database_response do
-      assert_equal @service.create_or_update_database(@data_hash), database_response
+      assert_equal @service.create_or_update_database(@database_hash), database_response
     end
   end
 
   def test_create_or_update_database_failure
     response = proc { raise MsRestAzure::AzureOperationError.new(nil, nil, 'error' => { 'message' => 'mocked exception' }) }
     @databases.stub :create_or_update, response do
-      assert_raises(RuntimeError) { @service.create_or_update_database(@data_hash) }
+      assert_raises(RuntimeError) { @service.create_or_update_database(@database_hash) }
     end
   end
 end

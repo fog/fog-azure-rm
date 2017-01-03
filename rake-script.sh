edb6f374f11d9301b4a6e77ffd41cc8a8808ea1d
Samawia Moin <samawia.moin@confiz.com>
2017-01-03 10:50:33 +0500

    remove code from code climate line

--- rake-script.sh
+++ rake-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rake cc_coverage
 export CODECLIMATE_REPO_TOKEN="b1401494baa004d90402414cb33a7fc6420fd3693e60c677a120ddefd7d84cfd"
-codeclimate-test-reporter --directory /home/travis/build/fog/fog-azure-rm/coverage/
+codeclimate-test-reporter

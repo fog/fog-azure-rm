e4c08fca28a1a35e498a39f106869202d3f79506
Samawia Moin <samawia.moin@confiz.com>
2017-01-04 16:32:47 +0500

    Made peer review changes in load balancer request

--- lib/fog/azurerm/requests/network/list_load_balancers_in_subscription.rb
+++ lib/fog/azurerm/requests/network/list_load_balancers_in_subscription.rb
@@ -4,14 +4,14 @@ module Fog
       # Real class for Network Request
       class Real
         def list_load_balancers_in_subscription
-          msg = "Getting list of Load-Balancers in subscription"
+          msg = 'Getting list of Load-Balancers in subscription'
           Fog::Logger.debug msg
           begin
             load_balancers = @network_client.load_balancers.list_all
           rescue MsRestAzure::AzureOperationError => e
             raise_azure_exception(e, msg)
           end
-          Fog::Logger.debug "Successfully retrieved load balancers in subscription"
+          Fog::Logger.debug 'Successfully retrieved load balancers in subscription'
           load_balancers
         end
       end

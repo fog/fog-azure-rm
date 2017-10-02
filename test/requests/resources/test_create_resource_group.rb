b46ca86cc3ef3b6ed9ffbfc482cadbe677a70a57
bilal.naeem <bilal.naeem@confiz.com>
2017-10-02 17:34:33 +0500

    Fixed Hound offence

--- test/requests/resources/test_create_resource_group.rb
+++ test/requests/resources/test_create_resource_group.rb
@@ -6,19 +6,20 @@ class TestCreateResourceGroup < Minitest::Test
     @service = Fog::Resources::AzureRM.new(credentials)
     @client = @service.instance_variable_get(:@rmc)
     @resource_groups = @client.resource_groups
+    @tags = { tagkey1: 'tagvalue1' }
   end
 
   def test_create_resource_group_success
     mocked_response = ApiStub::Requests::Resources::ResourceGroup.create_resource_group_response(@client)
     @resource_groups.stub :create_or_update, mocked_response do
-      assert_equal @service.create_resource_group('fog-test-rg', 'west us', { tagkey1: 'tagvalue1' }), mocked_response
+      assert_equal @service.create_resource_group('fog-test-rg', 'west us', @tags), mocked_response
     end
   end
 
   def test_create_resource_group_failure
     response = proc { raise MsRestAzure::AzureOperationError.new(nil, nil, 'error' => { 'message' => 'mocked exception' }) }
     @resource_groups.stub :create_or_update, response do
-      assert_raises(RuntimeError) { @service.create_resource_group('fog-test-rg', 'west us', { tagkey1: 'tagvalue1' }) }
+      assert_raises(RuntimeError) { @service.create_resource_group('fog-test-rg', 'west us', @tags) }
     end
   end
 end

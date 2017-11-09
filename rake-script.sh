03798808915e721e7c1f62ddd04cd5b4f37d662b
Maham Nazir <30528870+maham-nazir-confiz@users.noreply.github.com>
2017-11-09 20:15:08 +0500

    Update .travis.yml File (#342)
    Updated travis and rakescript

--- rake-script.sh
+++ rake-script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 rake cc_coverage
+R=$?
 export CODECLIMATE_REPO_TOKEN="b1401494baa004d90402414cb33a7fc6420fd3693e60c677a120ddefd7d84cfd"
 codeclimate-test-reporter
+if [ $R == 1 ]; then
+  exit 1
+else
+  exit 0
+fi

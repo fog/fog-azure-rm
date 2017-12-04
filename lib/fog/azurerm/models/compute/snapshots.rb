305ac129c447d910de1680e8c4de638f0149b7a0
KevinLoiseau <k.loiseauand@gmail.com>
2017-12-04 14:40:20 +0100

    Add 'all' method to fog collection snapshots

--- lib/fog/azurerm/models/compute/snapshots.rb
+++ lib/fog/azurerm/models/compute/snapshots.rb
@@ -6,6 +6,18 @@ module Fog
       class Snapshots < Fog::Collection
         model Fog::Compute::AzureRM::Snapshot
         attribute :resource_group
+
+        def all
+          snapshots = if resource_group.nil?
+                        service.list_snapshots_in_subscription
+                      else
+                        requires :resource_group
+                        service.list_snapshots_by_rg(resource_group)
+                      end
+          snapshots = snapshots.map { |snapshot| Fog::Compute::AzureRM::Snapshot.parse snapshot }
+
+          load(snapshots)
+        end
       end
     end
   end

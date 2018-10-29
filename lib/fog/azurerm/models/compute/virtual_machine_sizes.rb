6e5e737543d526ad4eaaabaf557829d581cdcdf6
KevinLoiseau <k.loiseauand@gmail.com>
2018-10-29 17:39:37 +0100

    Add method all to VirtualMachineSizes

--- lib/fog/azurerm/models/compute/virtual_machine_sizes.rb
+++ lib/fog/azurerm/models/compute/virtual_machine_sizes.rb
@@ -4,6 +4,14 @@ module Fog
       class VirtualMachineSizes < Fog::Collection
         attribute :location
         model Fog::Compute::AzureRM::VirtualMachineSize
+
+        def all
+          requires :location
+          virtual_machine_sizes = service.list_virtual_machine_sizes(location).map do |virtual_machine_size|
+            Fog::Compute::AzureRM::VirtualMachineSize.parse(virtual_machine_size)
+          end
+          load(virtual_machine_sizes)
+        end
       end
     end
   end
--- test/models/compute/test_virtual_machine_sizes.rb
+++ test/models/compute/test_virtual_machine_sizes.rb
@@ -4,9 +4,30 @@ class TestVirtualMachineSizes < Minitest::Test
   def setup
     @service = Fog::Compute::AzureRM.new(credentials)
     @virtual_machine_sizes = Fog::Compute::AzureRM::VirtualMachineSizes.new(location: 'location', service: @service)
+    @compute_client = @service.instance_variable_get(:@compute_mgmt_client)
+    @response = ApiStub::Models::Compute::VirtualMachineSize.virtual_machine_sizes(@compute_client)
   end
 
   def test_collection_attributes
     assert_respond_to @virtual_machine_sizes, :location
+  end
+
+  def test_collection_methods
+    methods = %i(all)
+    methods.each do |method|
+      assert_respond_to @virtual_machine_sizes, method
+    end
+  end
+
+  def test_all_method_response
+    @service.stub :list_virtual_machine_sizes, @response do
+      sizes = @virtual_machine_sizes.all
+
+      assert_instance_of Fog::Compute::AzureRM::VirtualMachineSizes, sizes
+      assert sizes.size >= 1
+      sizes.each do |size|
+        assert_instance_of Fog::Compute::AzureRM::VirtualMachineSize, size
+      end
+    end
   end
 end

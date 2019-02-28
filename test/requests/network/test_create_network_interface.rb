b0fa8ff34d4300ce6ead15bbad208db1f47a3e10
Maham Nazir <30528870+maham-nazir-confiz@users.noreply.github.com>
2019-02-28 14:17:14 +0500

    Added Async Create Call for Network Interface (#414)
    The Network Interface Card can now be created via an Async call as well. 
    Respective unit tests and integration tests have been updated as well.

--- test/requests/network/test_create_network_interface.rb
+++ test/requests/network/test_create_network_interface.rb
@@ -14,12 +14,22 @@ class TestCreateNetworkInterface < Minitest::Test
     @network_interfaces.stub :create_or_update, mocked_response do
       assert_equal @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', 'fog-test-ip-address-id', 'fog-test-nsg-id', 'fog-test-ip-configuration', 'Dynamic', '10.0.0.8', ['id-1', 'id-2'], ['id-1', 'id-2'], @tags), mocked_response
     end
+
+    # Async
+    @network_interfaces.stub :create_or_update_async, mocked_response do
+      assert_equal @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', 'fog-test-ip-address-id', 'fog-test-nsg-id', 'fog-test-ip-configuration', 'Dynamic', '10.0.0.8', ['id-1', 'id-2'], ['id-1', 'id-2'], @tags, false, true), mocked_response
+    end
   end
 
   def test_create_network_interface_without_public_ip_success
     mocked_response = ApiStub::Requests::Network::NetworkInterface.create_network_interface_response(@network_client)
     @network_interfaces.stub :create_or_update, mocked_response do
       assert_equal @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', nil, 'fog-test-nsg-id', 'fog-test-ip-configuration', 'Dynamic', '10.0.0.8', ['id-1', 'id-2'], ['id-1', 'id-2'], @tags), mocked_response
+    end
+
+    # Async
+    @network_interfaces.stub :create_or_update_async, mocked_response do
+      assert_equal @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', nil, 'fog-test-nsg-id', 'fog-test-ip-configuration', 'Dynamic', '10.0.0.8', ['id-1', 'id-2'], ['id-1', 'id-2'], @tags, false, true), mocked_response
     end
   end
 
@@ -30,6 +40,13 @@ class TestCreateNetworkInterface < Minitest::Test
         @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', 'fog-test-ip-address-id', 'fog-test-nsg-id', 'fog-test-ip-configuration', ['id-1', 'id-2'], ['id-1', 'id-2'])
       end
     end
+
+    # Async
+    @network_interfaces.stub :create_or_update_async, response do
+      assert_raises ArgumentError do
+        @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', 'fog-test-ip-address-id', 'fog-test-nsg-id', 'fog-test-ip-configuration', ['id-1', 'id-2'], ['id-1', 'id-2'], false, true)
+      end
+    end
   end
 
   def test_create_network_interface_exception_failure
@@ -37,6 +54,13 @@ class TestCreateNetworkInterface < Minitest::Test
     @network_interfaces.stub :create_or_update, response do
       assert_raises MsRestAzure::AzureOperationError do
         @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', 'fog-test-ip-address-id', 'fog-test-nsg-id', 'fog-test-ip-configuration', 'Dynamic', '10.0.0.8', ['id-1', 'id-2'], ['id-1', 'id-2'], @tags)
+      end
+    end
+
+    # Async
+    @network_interfaces.stub :create_or_update_async, response do
+      assert_raises MsRestAzure::AzureOperationError do
+        @service.create_or_update_network_interface('fog-test-rg', 'fog-test-network-interface', 'West US', 'fog-test-subnet-id', 'fog-test-ip-address-id', 'fog-test-nsg-id', 'fog-test-ip-configuration', 'Dynamic', '10.0.0.8', ['id-1', 'id-2'], ['id-1', 'id-2'], @tags, false, true)
       end
     end
   end
